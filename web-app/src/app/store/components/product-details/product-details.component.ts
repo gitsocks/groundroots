e0cc5f5788bc3a9021adcc25db3ab01b422f38e1
Herco Bezuidenhout <herco@thinwood.co>
2021-02-09 14:17:07 +0200

    Updated products details branch

--- web-app/src/app/store/components/product-details/product-details.component.ts
+++ web-app/src/app/store/components/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
 import { Component, Input, OnChanges, OnInit, Output, SimpleChanges, EventEmitter } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        top: '20%'
+      })),
+      state('closed', style({
+        top: '100%'
+      })),
+      transition('open => closed', [
+        animate('0.3s')
+      ]),
+      transition('closed => open', [
+        animate('0.3s')
+      ]),
+    ]),
+  ]
 })
 export class ProductDetailsComponent implements OnInit, OnChanges {
 
