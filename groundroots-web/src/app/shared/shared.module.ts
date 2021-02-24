d2c1b9c2415a4904e77591f24d7e0adfb916070c
Herco Bezuidenhout <herco@thinwood.co>
2021-02-24 10:20:25 +0200

    Created box component

--- groundroots-web/src/app/shared/shared.module.ts
+++ groundroots-web/src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BoxComponent } from './components/box/box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BoxComponent],
   imports: [
     CommonModule,
     FormsModule,
