9715f2783e78c3fd0e3497cb318e65d49df7a96c
Herco Bezuidenhout <herco@thinwood.co>
2021-02-25 13:49:57 +0200

    converted to express app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
+const express = require('express');
+const cors = require('cors');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+
+app.get('/hello-world', (request: any, response: any) => {
+    response.send("Hello from Firebase")
+});
+
+exports.api = functions.https.onRequest(app);
