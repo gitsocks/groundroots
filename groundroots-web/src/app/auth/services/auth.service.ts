a4e4b62979c140a73916be488cfddaa63786c667
Herco Bezuidenhout <herco@thinwood.co>
2021-03-04 11:27:29 +0200

    created sign up function

--- groundroots-web/src/app/auth/services/auth.service.ts
+++ groundroots-web/src/app/auth/services/auth.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase'
+import { User } from 'src/app/shared/models/user/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,11 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  signUp(user: User) {
+    firebase.auth().createUserWithEmailAndPassword(user.email, user.password).then(credentials => {
+      console.log(credentials.user);
+    }).catch(error => { console.error(error) });
+  }
+
 }
